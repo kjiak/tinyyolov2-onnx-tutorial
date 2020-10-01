f95dca9dbc943f4d4480ec7a5be9d7a2d01d9a57
j <kjiakai@gmail.com>
2020-10-02 01:37:54 +0800

    Add project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# tinyyolov2-onnx-tutorial
+
+### Credits
+https://docs.microsoft.com/en-us/dotnet/machine-learning/tutorials/object-detection-onnx <br>
---
+++ tinyyolov2-onnx-tutorial/DataStructures/ImageNetPrediction.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace tinyyolov2_onnx_tutorial.DataStructures
+{
+    public class ImageNetPrediction
+    {
+        [ColumnName("grid")]
+        public float[] PredictedLabels;
+    }
+}
